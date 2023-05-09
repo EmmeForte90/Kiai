d20cb9f54a66568ed22a6d9649986373b3ac34f7
Beerman84 <cappelloeros@gmail.com>
2023-05-09 19:33:06 +0200

    nemico katana prima parte

--- NewKiai/Assets/nemico_katana.cs
+++ NewKiai/Assets/nemico_katana.cs
@@ -7,17 +7,22 @@ using Spine.Unity;
 public class nemico_katana : MonoBehaviour
 {
     private float horizontal;
-    private float velocita = 8f;
-    private float velocita_difesa = 4f;
-    private float velocita_totale;
+    private float velocita = 4f;
+    private float velocita_corsa = 6f;
     private bool bool_dir_dx = true;
-    private bool bool_difesa=true;
     private SkeletonAnimation skeletonAnimation;
-    private bool bool_movimento=true;
-    private bool bool_attacca_personaggio=false;
     public GameObject GO_player;
-    public float distanza_rottura=20f;
+    public float distanza_guardia=2f;
+    public float distanza_attacco=0.5f;
     private bool bool_morte_attiva=false;
+    private float distanza_temp;
+    private Vector2 xTarget;
+
+    private float tempo_attuale_guardia=0;
+
+    private string stato;
+
+    private float tempo_stanchezza;
     
     [SerializeField] private Vector3[] posizioni;
     private int index_posizioni;
@@ -30,45 +35,66 @@ public class nemico_katana : MonoBehaviour
         skeletonAnimation = GetComponent<SkeletonAnimation>();
     }
 
+    void Update(){
+        if (tempo_stanchezza>0){
+            print ("stanchezza: "+tempo_stanchezza);
+            tempo_stanchezza-=(1f*Time.deltaTime);
+            if (tempo_stanchezza>0){return;}
+            stato="idle";
+        }
+
+        distanza_temp=calcola_distanza((int)(GO_player.transform.position.x),(int)(GO_player.transform.position.y),(int)(transform.position.x),(int)(transform.position.y));
+        //print ("distanza: "+distanza_temp);
+
+        if (distanza_temp<distanza_guardia){
+            if (stato=="idle"){
+                stato="guardia";
+                tempo_attuale_guardia=1;
+            }
+            if (stato=="guardia"){
+                tempo_attuale_guardia-=(1f*Time.deltaTime);
+                if (tempo_attuale_guardia<=0){
+                    stato="puo_attaccare";
+                }
+            }
+            if (stato=="puo_attaccare"){
+                if (distanza_temp<distanza_attacco){
+                    stato="attacco";
+                }
+            }
+        }
+        else {
+            stato="idle";
+        }
+    }
+
+    private IEnumerator ferma_attacco(){    
+        yield return new WaitForSeconds(0.25f);
+        stato="tired";
+        tempo_stanchezza=2.5f;
+    }
+
 
     private void FixedUpdate()
     {
+        //print ("stato: "+stato);
+        if (tempo_stanchezza>0){
+            if (stato=="tired"){
+                skeletonAnimation.AnimationName = "tired";
+            }
+            return; 
+        }
         if (bool_morte_attiva){return;}
-        //print(transform.rotation+" - "+launchoffset.rotation+" - "+bool_dir_dx);
-        velocita_totale=velocita;
-        if (bool_difesa){velocita_totale=velocita_difesa;}
-        
-        if (
-            calcola_distanza(
-                (int)(GO_player.transform.position.x),
-                (int)(GO_player.transform.position.y),
-                (int)(transform.position.x),
-                (int)(transform.position.y)
-            )<distanza_rottura){
-            bool_movimento=false;
-            bool_difesa=true;
-            bool_attacca_personaggio=true;
-            //skeletonAnimation.AnimationName = "shoot";
-            if (transform.position.x<GO_player.transform.position.x){horizontal=1;}
-            else {horizontal=-1;}
-            
-            float animTime = skeletonAnimation.state.GetCurrent(0).AnimationTime;
-            
-            Flip();
-            return;
-        }
-        else {bool_movimento=true;bool_attacca_personaggio=false;bool_difesa=false;}
-        
-        if (bool_attacca_personaggio){
-        }
-        else {
-            if (bool_movimento){
-                if (!bool_difesa){skeletonAnimation.AnimationName = "walk_thief";}
-                else {skeletonAnimation.AnimationName = "run";}
 
-
-                /*
-                transform.position = Vector2.MoveTowards(transform.position,posizioni[index_posizioni], Time.deltaTime*velocita_totale);
+        switch (stato){
+            case "attacco":{
+                skeletonAnimation.AnimationName = "horizzontal/attack_horizzontal";
+                StartCoroutine(ferma_attacco());
+                break;  
+            }
+            case "idle":{
+                skeletonAnimation.AnimationName = "walk_thief";
+                transform.position = Vector2.MoveTowards(transform.position,posizioni[index_posizioni], Time.deltaTime*velocita);
                 if (transform.position.x<posizioni[index_posizioni].x){horizontal=1;}
                 else {horizontal=-1;}
                 
@@ -77,19 +103,33 @@ public class nemico_katana : MonoBehaviour
                         index_posizioni=0;
                     } else {index_posizioni++;}
                 }
-                */
                 Flip();
-            } else {
-                skeletonAnimation.AnimationName = "idle";
+                break;
+            }
+            case "guardia":{
+                if (transform.position.x<GO_player.transform.position.x){horizontal=1;}
+                else {horizontal=-1;}
+                skeletonAnimation.AnimationName = "idle_battle";
+                Flip();
+                break;
+            }
+            case "puo_attaccare":{
+                if (transform.position.x<GO_player.transform.position.x){horizontal=1;}
+                else {horizontal=-1;}
+                skeletonAnimation.AnimationName = "run_thief";
+                xTarget = new Vector2(GO_player.transform.position.x, transform.position.y);
+                transform.position = Vector2.MoveTowards(transform.position,xTarget, Time.deltaTime*velocita_corsa);
+                Flip();
+                break;
             }
         }
-        
-        //rb.velocity = new Vector2(horizontal * velocita_totale, rb.velocity.y);
+
+        return;
     }
 
     private void Flip()
     {
-        if (bool_dir_dx && horizontal < 0f || !bool_dir_dx && horizontal > 0f)
+        if (bool_dir_dx && horizontal > 0f || !bool_dir_dx && horizontal < 0f)
         {
             bool_dir_dx = !bool_dir_dx;
             Vector3 localScale = transform.localScale;
