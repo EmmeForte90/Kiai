d51878cc71f9a615d3aa258d6c5c1cbe410c0abb
EmmeForte90 <mauroemme90@gmail.it>
2023-07-11 20:57:56 +0200

    Fix

--- NewKiai/Assets/Script/HealItem.cs
+++ NewKiai/Assets/Script/HealItem.cs
@@ -8,10 +8,24 @@ public class HealItem : MonoBehaviour
     [SerializeField] GameObject Explode;
    // [SerializeField] Transform prefabExp;
     [SerializeField] int heal = 50;
+    private GameObject target;
 
     [SerializeField] float lifeTime = 0.2f;
     [SerializeField] Rigidbody2D rb;
+    public float Speed = 5f;
 
+void Start()
+    {
+        target = GameObject.Find("Nekotaro");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        transform.position += (direction * (Speed * Time.deltaTime));
+    }
 
 
      void OnTriggerEnter2D(Collider2D other) 
